0c08725922f5390298a91ea520f8f04955e32c31
Misenpai <misenpai200231@gmail.com>
2025-06-02 16:05:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import { Client, GatewayIntentBits, Collection } from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Stride is online as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);

d81f338c5f7fd11f52ae6f928a7b172b9e6dc56d
Misenpai <misenpai200231@gmail.com>
2025-06-04 11:17:54 +0530

    config2

--- config.js
+++ config.js
@@ -4,5 +4,8 @@ export const BOT_TOKEN = process.env.DISCORD_TOKEN;
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const GUILD_ID = process.env.GUILD_ID;
 
+export const LOCKED_CHANNELS = ["1379051046587863061", "1379051086551449692"];
+
+export const CELEBRATION_CHANNEL_ID = "1379051676874309794";
 export const DEFAULT_FOCUS_MINUTES = 25;
 export const MAX_FOCUS_MINUTES = 120;

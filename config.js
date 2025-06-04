4672abaf603569fd03ed258421800584b3105113
Misenpai <misenpai200231@gmail.com>
2025-06-04 11:14:38 +0530

    config

---
+++ config.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+export const BOT_TOKEN = process.env.DISCORD_TOKEN;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const GUILD_ID = process.env.GUILD_ID;
+
+export const DEFAULT_FOCUS_MINUTES = 25;
+export const MAX_FOCUS_MINUTES = 120;
